f7823e62a480796b84d7cd7af4ffdbb6718dac0d
Ramya <ramyakongara12345@gmail.com>
2025-03-15 21:33:44 -0400

    my first commit

---
+++ ceaser_cipher.py
@@ -0,0 +1,12 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(original_text, shift_amount):
+    cipher_text = "".join(alphabet[(alphabet.index(letter) + shift_amount) % len(alphabet)] if letter in alphabet else letter for letter in original_text)
+    print(cipher_text)
+
+encrypt(original_text=text, shift_amount=shift)
+
